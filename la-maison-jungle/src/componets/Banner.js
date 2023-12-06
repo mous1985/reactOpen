740ecff858f9d0a3fa12de0add0577a7131c511e
mus_36 <mustapha.benazzouz.213@gmail.com>
2023-12-06 16:04:05 +0100

    create foldeer style with banner.css and cart.css for customiz ouwer component

--- la-maison-jungle/src/componets/Banner.js
+++ la-maison-jungle/src/componets/Banner.js
@@ -1,5 +1,25 @@
+import '../style/Banner.css'
+import logo from '../assets/logo.png'
+
 function Banner() {
-    return <h1>La maison jungle</h1>
+    const title = 'La maison jungle'
+    return (
+            <div className='lmj-banner'>
+                <img src={logo} alt='La maison jungle' className='lmj-logo' />
+                <h1 className='lmj-title'>{title}</h1>
+            </div>)
+            /* or we can use style as object to replace classeName  "for the good parctics it's recomandat to use classeName" */
+            /* <div 
+                    style={{
+                        color: 'black',
+                        textAlign: 'right',
+                        padding: 32,
+                        borderBottom: 'solid 3px solid black',
+                    }}
+            >
+                <h1>La maison jungle</h1>
+            </div>  */
 }
+
 
 export default Banner
